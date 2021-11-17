62577b660cdff3ff7638244f664a34717bab258d
VanishingTacos <devonstuper@gmail.com>
2021-11-17 02:47:14 -0500

    Cleanup video file after using

--- reverse_last_tweet.py
+++ reverse_last_tweet.py
@@ -4,6 +4,7 @@ import re
 import sys
 import urllib.request
 import ffmpeg
+from PIL import Image, ImageOps
 from auth import *
 
 # Authentication
@@ -13,24 +14,24 @@ api = tweepy.API(auth)
 
 # Authentication check
 if not api.verify_credentials():
-    sys.exit("Authentication ERROR!")
+    sys.exit('Authentication ERROR!')
 
-tweet = api.user_timeline(screen_name = userID, count = 1, exclude_replies=True, tweet_mode = "extended", include_rts = False)[0] # Pulls latest tweet from users timeline that is not a reply or rt
+tweet = api.user_timeline(screen_name = userID, count = 1, exclude_replies=True, tweet_mode = 'extended', include_rts = False)[0] # Pulls latest tweet from users timeline that is not a reply or rt
 
 # Check log for the latest entry
-with open(log, "rb") as f:
+with open(log, 'rb') as f:
     f.seek(2, os.SEEK_END)
     while f.read(1) != b'\n':
         f.seek(-2, os.SEEK_CUR)
     last_tweet_id = f.readline().decode()
 
 # If latest current tweet ID matches latest log entry then do nothing, else post reversed tweet
-if str(tweet.id) + "\n" == last_tweet_id:
-    print("Nothing to do")
+if str(tweet.id) + '\n' == last_tweet_id:
+    print('Nothing to do')
 else:
     # open log and appends new tweet ID
-    openLog = open(log, "a")
-    openLog.write(str(tweet.id) + "\n")
+    openLog = open(log, 'a')
+    openLog.write(str(tweet.id) + '\n')
     openLog.close()
     tweetLen = len(tweet.full_text) # calculate the tweets length
     removeURL = re.sub(r'http\S+', '',tweet.full_text) # Removes URL from the tweet if one is found
@@ -38,13 +39,13 @@ else:
 
     # try to get video info. if none is found then skip
     try:
-        media_info = tweet.extended_entities["media"][0]["video_info"]
-        if media_info["variants"][0]["content_type"] == "video/mp4":
-            video_link = media_info["variants"][0]["url"] # get video url
-            urllib.request.urlretrieve(video_link, media + "video0.mp4") # download video
-            in_file = ffmpeg.input(media + "video0.mp4") # input file for ffmpeg
-            reversed_video = in_file.video.filter("reverse") # reverse video
-            reversed_audio = in_file.audio.filter("areverse") # reverse audio
+        media_info = tweet.extended_entities['media'][0]['video_info']
+        if media_info['variants'][0]['content_type'] == 'video/mp4':
+            video_link = media_info['variants'][0]['url'] # get video url
+            urllib.request.urlretrieve(video_link, media + 'video0.mp4') # download video
+            in_file = ffmpeg.input(media + 'video0.mp4') # input file for ffmpeg
+            reversed_video = in_file.video.filter('reverse') # reverse video
+            reversed_audio = in_file.audio.filter('areverse') # reverse audio
             (
                 ffmpeg
                 .concat(reversed_video,reversed_audio, v=1, a=1) # concats reversed audio and video
@@ -52,27 +53,34 @@ else:
                 .global_args('-loglevel' , 'quiet') # shut up ffmpeg
                 .run()
             )
-            upload = api.media_upload(media + "out0.mp4", chunked = True, media_category = "tweet_video") # upload video
-            api.update_status(slicedTweet, in_reply_to_status_id = tweet.id, auto_populate_reply_metadata = True, media_ids=[upload.media_id_string]) # post reply with video
+            upload = api.media_upload(media + 'out0.mp4', chunked = True, media_category = 'tweet_video') # upload video
+            api.update_status(slicedTweet, media_ids=[upload.media_id_string]) # post reply with video
             # remove videos
-            os.remove(media + "video0.mp4")
-            os.remove(media + "out0.mp4")
-            print("Done")
+            os.remove(media + 'video0.mp4')
+            os.remove(media + 'out0.mp4')
 
     except:
-        # check if tweet contains a url
-        if tweet.entities['urls']:
-            for url in tweet.entities['urls']:
-                # if expanded_url is equal to twitch link then get tweet link and retweet
-                if re.match(r'http\S+\/\/[Tt]witch.tv/samwitch', url['expanded_url']):
+        try:
+            if tweet.extended_entities['media'][0]['type'] == 'photo':
+                urllib.request.urlretrieve(tweet.extended_entities['media'][0]['media_url_https'], media + 'image.jpg') # download image
+                image = Image.open( media + 'image.jpg') # open image
+                image_mirror = ImageOps.mirror(image) # mirror image
+                image_mirror.save(media + 'mirror.jpg', quality=100) # save image
+                upload = api.media_upload(media + 'mirror.jpg', chunked = True, media_category = 'tweet_image') # upload image
+                api.update_status(slicedTweet + '\n\nhttps://twitter.com/' + userID + '/statuses/' + str(tweet.id), media_ids=[upload.media_id_string]) # post with image
+                os.remove(media + 'mirror.jpg')
+        except:
+            # check if tweet contains a url
+            if tweet.entities['urls']:
+                for url in tweet.entities['urls']:
+                    # if expanded_url is equal to twitch link then get tweet link and retweet
+                    if re.match(r'http\S+\/\/[Tt]witch.tv/samwitch', url['expanded_url']):
 
-                    liveLink = ("https://twitter.com/" + userID + "/statuses/" + str(tweet.id))
-                    api.update_status(slicedTweet + "\n\n" + liveLink )
+                        liveLink = ('https://twitter.com/' + userID + '/statuses/' + str(tweet.id))
+                        api.update_status(slicedTweet + '\n\n' + liveLink )
                     
-                else:
-                    # else if tweet has link and is not a twitch link then just reply
-                    api.update_status(slicedTweet, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True) # reply to tweet
-        else:
-            api.update_status(slicedTweet, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True) # reply to tweet
-
-        print("Done")
+                    else:
+                        # else if tweet has link and is not a twitch link then just reply
+                        api.update_status(slicedTweet, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True) # reply to tweet
+            else:
+                api.update_status(slicedTweet, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True) # reply to tweet
